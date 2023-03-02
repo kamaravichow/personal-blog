2b33deef87bf345786bc97c9bbfa02fa3ffa459d
Aravind Chowdary <kamaravichow@gmail.com>
2023-03-02 08:23:41 +0530

    added adblock protect

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,21 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
+      {/* Ad block protect */}
+      {/* <script async src="https://fundingchoicesmessages.google.com/i/pub-7463578811537182?ers=1" nonce="dTFROAoAxoV8QMVH0vlhfw"></script>
+      <script nonce="dTFROAoAxoV8QMVH0vlhfw">(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script> */}
+      <Script
+        src='https://fundingchoicesmessages.google.com/i/pub-7463578811537182?ers=1'
+        nonce='dTFROAoAxoV8QMVH0vlhfw'
+        strategy='lazyOnload'
+      />
+      <Script
+        nonce='dTFROAoAxoV8QMVH0vlhfw'
+        dangerouslySetInnerHTML={{
+          __html: `(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`,
+        }}
+        strategy='lazyOnload'
+      />
       <Script
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7463578811537182`}
         strategy="lazyOnload"
